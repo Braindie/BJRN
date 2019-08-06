653c64984360db772531aaaf6bc27a47d19eb670
zhangwenjun <zhangwenjun@micego.com>
2019-08-06 18:15:34 +0800

    React Navigation

--- MyRNApp/index.ios.js
+++ MyRNApp/index.ios.js
@@ -9,25 +9,34 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  FlatList
 } from 'react-native';
 
 export default class MyRNApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: ['1', '2', '3']
+    }
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <FlatList
+        data={this.state.data}
+        renderItem={this.renderCell}
+        keyExtractor={item => item.id}
+      />
     );
+  }
+
+  renderCell({ item }) {
+    return (
+      <View>
+        <Text>{item}</Text>
+      </View>
+    )
   }
 }
 
